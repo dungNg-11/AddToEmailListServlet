96ffdbd9924ea217d85b457550a7ae62719cd879
Nguyen Dung <64003306+dungNg-11@users.noreply.github.com>
2022-01-05 22:28:17 +0700

    test edit code

--- src/edu/fa/model/User.java
+++ src/edu/fa/model/User.java
@@ -2,8 +2,8 @@ package edu.fa.model;
 
 public class User
 {
-	private String firstName;
-	private String lastName;
+//	private String firstName;
+//	private String lastName;
 	private String emailAddress;
 	public User()
 	{
